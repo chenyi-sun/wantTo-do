9b886e2589d0d78af0c82bdd58639a659f57160f
/ </>
2017-05-25 13:32:14 +0800

    add

---
+++ animation-algori/setAnimation.js
@@ -0,0 +1,7 @@
+//http://www.zhangxinxu.com/wordpress/2013/09/css3-animation-requestanimationframe-tween-%E5%8A%A8%E7%94%BB%E7%AE%97%E6%B3%95/
+(function(){
+    var venders = ['webkit', 'moz'];
+    for(var i = 0; x< venders.length){
+
+    }
+})();

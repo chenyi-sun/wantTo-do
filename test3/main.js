b115f72bb8997a1e69d8cffc0fc7a488b5dbee60
/ </>
2017-06-28 11:45:19 +0800

    improve

--- test3/main.js
+++ test3/main.js
@@ -1,23 +1,22 @@
-console.log('sss');
 class pariticale{
   constructor(top,left){
     this.top = top;
     this.left = left;
-    this.r = Math.random()*2;
+    this.r = Math.random()*6;
     this.moveX = Math.random();
     this.moveY = Math.random();
+    this.moveX = this.moveX>=0.5?this.moveX:-this.moveX;
+    this.moveY = this.moveY>=0.5?this.moveY:-this.moveY;
   }
   drawCircle(box){
     var x = this.left;
     var y = this.top;
     var r = this.r;
-    // if(x>width||x<0){
-    //   console.log('sss ddd');
-    // }
+
     box.beginPath();
     box.arc(x, y ,r, 0, 360);
     box.closePath();
-    box.fillStyle = "rgba(204,204,204,0.3)";
+    box.fillStyle = "rgba(204,204,204,0.2)";
     box.fill();
   }
   drawPath(thisbox,item){
@@ -29,27 +28,58 @@ class pariticale{
       box.moveTo(thisbox.left,thisbox.top);
       box.lineTo(item.left,item.top);
       box.closePath();
-      box.strokeStyle="rgba(204,204,204,0.3)";
+      box.strokeStyle="rgba(204,204,204,0.2)";
       box.stroke();
       }
-    }
+  }
   move(){
-    this.top += this.moveX;
-    this.left += this.moveY
-    if(this.top> width||this.top < 0){
+    if(this.left> width||this.top < 0){
         this.moveX = -this.moveX;
     }
     if(this.top> height||this.top < 0){
        this.moveY = -this.moveY;
     }
+    this.top += this.moveY/2;
+    this.left += this.moveX/2;
   }
 }
 
+class CenterCircle extends pariticale{
+  constructor(top,left){
+    super(top,left);
+  }
+  drawCircle(){
+    var x = this.left;
+    var y = this.top;
+    var r = 2;
+
+    box.beginPath();
+    box.arc(x, y ,r, 0, 360);
+    box.closePath();
+    box.fillStyle = "rgba(255, 77, 54,0.8)";
+    box.fill();
+  }
+   drawPath(thisbox,item){
+    let widthPath = thisbox.left - item.left;
+    let heightPath = thisbox.top - item.top;
+    var dr = Math.sqrt(widthPath*widthPath + heightPath*heightPath);
+    if(dr < 120){
+      box.beginPath();
+      box.moveTo(thisbox.left,thisbox.top);
+      box.lineTo(item.left,item.top);
+      box.closePath();
+      box.strokeStyle="rgba(255, 77, 54,0.2)";
+      box.stroke();
+      }
+  }
+}
 let circles = [];
 let canvas = document.getElementsByTagName('canvas')[0];
 let box = canvas.getContext('2d');
 let height = canvas.height = canvas.offsetHeight;
 let width = canvas.width = canvas.offsetWidth;
+let currentCanvas = new CenterCircle(0,0);
+
 
 function init(values){
   for(let i = 0; i<=values; ++i){
@@ -62,16 +92,27 @@ function init(values){
 
 function draw(){
   box.clearRect(0,0,width,height);
+  currentCanvas.drawCircle();
+  // currentCanvas.drawPath();
   circles.forEach((item)=>{
     item.drawCircle(box);
     let thisBox = item;
     circles.forEach((item)=>{
-      item.drawPath(thisBox,item)
+      item.drawPath(thisBox,item);
     });
     item.move();
-
+  });
+  circles.forEach((item)=>{
+    currentCanvas.drawPath(currentCanvas,item);
   });
   requestAnimationFrame(draw);
 
 }
 init(120);
+
+window.onmousemove = function(e){
+  let es = e||window.event;
+  currentCanvas.left = es.clientX;
+  currentCanvas.top = es.clientY;
+  console.log('sss');
+}

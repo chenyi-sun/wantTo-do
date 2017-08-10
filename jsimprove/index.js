4b8136d87ee274ad72f42ea4e97ef47c1b15535c
/ </>
2017-08-10 10:06:02 +0800

    improve

--- jsimprove/index.js
+++ jsimprove/index.js
@@ -90,5 +90,109 @@ try{
 catch(e){
     console.log(e);
 }
+//try catch尝试执行某个操作，如果有问题就报错结束//
+
+
+//prototype的基本用法
+function Range(from, to){
+    this.x = from;
+    this.y = to;
+    this.create = function(){
+        return this.x + this.y;
+    }
+}
+var rangeObject = new Range(2,3);
+console.log(rangeObject.create());
+
+function rangarr(from,to){
+    this.x = from;
+    this.y = to;
+}
+
+rangarr.prototype = {
+    plus: function(){
+        return this.x + this.y;
+    },
+    cut: function(){
+        return this.x - this.y;
+    }
+}
+
+var ran = new rangarr(10,20);
+console.log(ran.plus());
+console.log(ran.cut());
+//prototype的基本用法结束//
+
+
+//split，快速拼接数组
+var array1 = "ddd,sss,dddd";
+var array2 = array1.split(",");
+console.log(array2);
+//split，快速拼接数组结束//
+
+
+//indexof的用法
+var ask = 'djsjs,jsjsj';
+var nsws = ['ddd', 'dsss', 'dksksk'];
+console.log(ask.indexOf(','));
+console.log(nsws.indexOf('tt'));
+//indexof的用法结束//
+
+
+//call继承方法
+function A(x){
+    this.x = x;
+} 
+A.prototype.a = function(){
+    return this.x;
+};
+A.prototype.d = 'ssss';
+
+function B(x){
+    this.y = x;
+    A.call(this);
+}
+B.prototype.c = 'd';
+
+var a = new B('2'); 
+console.log(a.a); 
+//call继承A构造函数的this属性和方法，不继承prototype的方法
+
+//prototype的方法继承
+function C(x){
+    A.call(this,2);
+    this.y = x+2;
+    this.x = 'dddddd';
+}
+C.prototype = new A();
+var c = new C('333');
+console.log(c.x);
 
 //
+function D(x){
+    this.y = x+'23'+'44';
+}
+D.prototype = new A();
+// D.prototype.constructor = A;
+D.prototype.dd = function(){
+    console.log('dd');
+
+}
+var c = new D('333');
+console.log(c.x);
+console.log(c.d);
+c.dd();
+c.__proto__.new = 'ddd';
+console.log(c.__proto__);
+console.log(D.prototype.constructor);
+console.log(D.prototype);
+
+var s = {
+    ss: function(){
+        this.ask = 'ddd';
+        C.call(this,2);
+    }
+}
+var sd = new s.ss();
+console.log(sd.x);
+console.log(s.__proto__);

cdeed3ed6b33a31636f1250f3022bd2e849cf87a
/ </>
2017-08-24 18:47:56 +0800

    add

---
+++ requireModule/js/app-config.js
@@ -0,0 +1,16 @@
+ console.log('ssss');
+ require.config({
+             paths: {
+                //  ScrollMagic: 'https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/ScrollMagic.min',
+                //  debug: 'js/res/debug.addIndicators',
+                 vue: './vue',
+                 jquery: './jquery-1.8.1',
+                 loadash: './lodash',
+                 lhgdialog: './lhgdialog',
+                //  ScrollMagicJquery: 'https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/jquery.ScrollMagic.min',
+                //  TweenLite: 'https://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.0/TweenLite.min',
+                //  TweenMax: 'https://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.0/TweenMax.min',
+                //  TimelineMax: 'https://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.0/TimelineMax.min',
+                //  ScrollMagicGSAP: 'https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/animation.gsap.min',
+             }
+});
